384ce9060002db86f5372b7ed6434a81914234bd
Kevin K <kbknapp@gmail.com>
2014-08-26 22:05:01 -0400

    Added baseline script

---
+++ arch/debloat.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pacman -Rns gnome-robots gnome-nibbles xnoise empathy anjuta aisleriot accerciser gnome-sudoku gnome-mahjongg  four-in-a-row five-or-more evolution gnome-klotski iagno gnome-mines polari quadrapassel tali swell-foop transmission-cli
