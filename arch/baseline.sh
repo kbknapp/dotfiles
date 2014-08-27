8608de69de81d13d8345e3bed2958e13f92a2a82
Kevin K <kbknapp@gmail.com>
2014-08-27 22:32:41 +0000

    Added gnome-chess and gnome-tetravex to debloat, bug fixes for baseline.sh

--- arch/baseline.sh
+++ arch/baseline.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-
+VERSION="0.1.2"
 # Setup pacman mirrors
+echo "############################"
+echo "## Kustom Baseline v$VERSION"
+echo "############################"
+echo
 echo "Enabling multilib repo"
 sudo sh -c "echo \"[multilib]\" >> /etc/pacman.conf"
 sudo sh -c "echo \"Include = /etc/pacman.d/mirrorlist\" >> /etc/pacman.conf"
 cd /etc/pacman.d
 echo "Downloading the latest US mirrors"
-sudo curl -LSso mirrorlist.backup https://www.archlinux.org/mirrorlist/\?country\=US
-sudo sh -c "sed -e \"s/^#Server/Server/g\" mirrorlist.backup > mirrorlist.backup"
+sudo mv mirrorlist mirrorlist.backup
+sudo curl -LSso mirrorlist.tmp https://www.archlinux.org/mirrorlist/\?country\=US
+sudo sh -c "sed -e \"s/^#Server/Server/g\" mirrorlist.tmp > mirrorlist-us"
 echo "Listing Arch mirrors by speed"
-sudo sh -c "rankmirrors -n 6 mirrorlist.backup > mirrorlist"
-sudo mv antergos-mirrorlist > antergos-mirrorlist.backup
+sudo sh -c "rankmirrors -n 6 mirrorlist-us > mirrorlist"
+sudo rm mirrorlist.tmp
+sudo mv antergos-mirrorlist antergos-mirrorlist.backup
 echo "Listing Antergos mirrors by speed"
 sudo sh -c "rankmirrors -n 6 antergos-mirrorlist.backup > antergos-mirrorlist"
 echo "Updating local mirror repo"
@@ -20,16 +26,15 @@ sudo pacman -Syu --noconfirm
 
 # Debloat the system
 echo "Removing bloat software"
-sudo pacman -Rns gnome-robots gnome-nibbles xnoise empathy anjuta aisleriot accerciser gnome-sudoku gnome-mahjongg  four-in-a-row five-or-more evolution gnome-klotski iagno gnome-mines polari quadrapassel tali swell-foop transmission-cli --noconfirm
+sudo pacman -Rns gnome-robots gnome-chess gnome-tetravex gnome-nibbles xnoise empathy anjuta aisleriot accerciser gnome-sudoku gnome-mahjongg  four-in-a-row five-or-more evolution gnome-klotski iagno gnome-mines polari quadrapassel tali swell-foop transmission-cli --noconfirm
 
 # Install common software
 echo "Installing required software"
-sudo pacman -S go tree vim mercurial zsh firefox gauke ncdu terminator emacs --noconfirm
+sudo pacman -S go tree vim mercurial zsh firefox guake ncdu terminator emacs --noconfirm
 
 # Setup home dir
 cd ~
-echo "Creating required directories"
-mkdir -p .go/{bin,src/github.com,pkg}
+echo "Creating required home directories"
 mkdir Projects
 
 # git configurations
@@ -40,12 +45,12 @@ git config --global color.ui true
 
 # Clone my repos
 echo "Checking for dotfiles"
-if [ ! -d .dotfiles ]; then
+if [[ ! -d .dotfiles ]]; then
 	echo "Cloning dotfiles repo"
 	git clone https://github.com/kbknapp/dotfiles .dotfiles
 fi
 echo "Checking for oh-my-zsh repo"
-if [! -d .oh-my-zsh]; then
+if [[ ! -d .oh-my-zsh ]]; then
 	echo "Cloning oh-my-zsh repo"
 	git clone https://github.com/kbknapp/oh-my-zsh .oh-my-zsh
 fi
@@ -53,34 +58,41 @@ fi
 # Setup ZSH
 echo "Setting up ZSH"
 cp .oh-my-zsh/templates/zshrc-linux.zsh-template .zshrc
-sudo git clone https://github.com/kbknapp/oh-my-zsh .oh-my-zsh
-sudo cp .oh-my-zsh/templates/zshrc-linux.zsh-template .zshrc
-chsh -s zsh whoami
-sudo chsh -s zsh whoami
+sudo cd /root
+sudo git clone https://github.com/kbknapp/oh-my-zsh /root/.oh-my-zsh
+sudo cp /root/.oh-my-zsh/templates/zshrc-linux.zsh-template /root/.zshrc
+sudo chsh -s /bin/zsh ${whoami}
+cd ~
+chsh -s /bin/zsh ${whoami}
 
 # Setup GNOME Startup Apps (guake specifically)
+cd ~
 echo "Configuring GNOME startup applications"
 mkdir -p .config/autostart
 cd .config/autostart
 cp ~/.dotfiles/gnome/guake.desktop ~/.config/autostart
 
 # Setting up Go
+cd ~
+mkdir -p .go/{bin,src/github.com,pkg}
+export GOPATH=$HOME/.go
 # Used for stale packages in Go 1.3.1
-su -c "go install std"
+sudo go install std
 go get code.google.com/p/go.tools/cmd/goimports
 
 # Setting up Rust
+cd ~
 mkdir -p Projects/rust-nightlies
+mkdir -p .rust/{bin,pkg,src/github.com}
 cd ~/Projects/rust-nightlies
 curl -LSso rustup.sh https://static.rust-lang.org/rustup.sh
 chmod +x rustup.sh
 sudo ./rustup.sh
-cd ~
-mkdir -p .rust/{bin,pkg,src/github.com}
 
 # AUR Insalls (Maybe)
 #yaourt -S pycharm-community
 #yaourt -S sublime-text-dev
 #yaourt -S haroopad
+#yaourt -S conky-nvidia
 
 echo "Complete!"
