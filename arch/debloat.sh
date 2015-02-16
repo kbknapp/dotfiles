b77f4205b030c6979d541fafbb0146154e135a9d
Kevin K <kbknapp@gmail.com>
2015-02-16 18:25:07 -0500

    Moved baseline to bin folder and removed arch folder

--- arch/debloat.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pacman -Rns gnome-chess gnome-tetravex gnome-robots gnome-nibbles xnoise empathy anjuta aisleriot accerciser gnome-sudoku gnome-mahjongg  four-in-a-row five-or-more evolution gnome-klotski iagno gnome-mines polari quadrapassel tali swell-foop transmission-cli
