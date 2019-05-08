dad42179b146075ac1c04a3d0ab462cd088fc5b6
Kevin K <kbknapp@gmail.com>
2019-05-08 15:29:06 -0400

    adds vim setup to xubuntu baseline

--- bin/baseline_xubuntu.sh
+++ bin/baseline_xubuntu.sh
@@ -20,6 +20,7 @@ sudo apt install -y \
     snapd \
     dconf-cli \
     shellcheck \
+    neovim \
     build-essential gcc make pkg-config cmake automake libssl-dev
 
 # clone dotifles if they don't exist yet
@@ -39,6 +40,20 @@ fi
 # shellcheck source=../../.cargo/env
 source "${HOME}/.cargo/env"
 rustup default nightly
+
+# vim with Vundle
+cp "${HOME}/.dotfiles/vim/vimrc-linux" "${HOME}/.vimrc"
+mkdir -p "${HOME}/.vim/bundle"
+git clone https://github.com/VundleVim/Vundle.vim "${HOME}/.vim/bundle"
+vim +PluginInstall +qall
+
+# NeoVIM with Plug
+cp "${HOME}/.dotfiles/vim/vimrc-linux" "${HOME}/.vimrc"
+mkdir -p "${HOME}/.config/nvim"
+cp "${HOME}/.dotfiles/nvim/init.vim" "${HOME}/.config/nvim/"
+cp "${HOME}/.dotfiles/vim/vimrc-linux" "${HOME}/.vimrc"
+curl -fLo "${HOME}/.local/share/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +PlugInstall +qall
 
 pip install --user rtv
 
