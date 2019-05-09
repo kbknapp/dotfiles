172ee63dde4c8bca7d765ac3f707c2686eb3dfbd
Kevin K <kbknapp@gmail.com>
2019-05-09 13:40:08 -0400

    adds some common aliases and abbr's to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,18 @@
 fish_vi_key_bindings
+
+alias vim='nvim'
+
+# git settings
+abbr gl 'git log --graph --all --oneline --decorate'
+abbr gpo 'git push origin'
+abbr gst 'git status'
+
+# Exa
+abbr ls 'exa'
+abbr tree 'exa -T'
+
+abbr mkdir 'mkdir -pv'
+
+function ggpull
+    git pull origin (git branch | grep \* | awk '{print $2}')
+end
