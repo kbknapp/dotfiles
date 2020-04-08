0069a23bed89b72f610cd37605a5dba892909f38
Kevin K <kbknapp@gmail.com>
2020-04-07 23:47:17 -0400

    removes parts that have impl'ed

--- i3/xubuntu/xubuntu_setup.sh
+++ i3/xubuntu/xubuntu_setup.sh
@@ -1,40 +1,3 @@
-#!/bin/bash
-
-# exit on error
-set -e
-
-# Set this!
-MACHINE=
-
-if [ -z "${MACHINE}" ]; then
-    echo "No '$MACHINE' set"
-    exit 1
-fi
-
-# Deps?
-#
-# libxtst-dev libx11-dev libxtst-dev  \
-#    libxcb-image0-dev \
-#   libxcb-randr0-dev libxcb-shape0-dev  \
-#   libxcb-composite0-devb \
-#   libxcb-ewmh2 libxcb-randr0-dev libev-dev libxcb-xinerama0-dev \
-
-
-BUILD_DIR=$HOME/.build
-NPROC=4
-DOT_I3_XUBUNTU=$HOME/.dotfiles/i3/xubuntu/
-COMMON_HOME="${DOT_I3_XUBUNTU}/common/home"
-MACHINE_HOME="${DOT_I3_XUBUNTU}/${MACHINE}/home"
-
-sudo apt install -y git
-
-# clone dotifles if they don't exist yet
-if [ ! -d "${HOME}/.dotfiles" ]; then
-    git clone https://github.com/kbknapp/dotfiles .dotfiles
-fi
-
-bash "${HOME}"/.dotfiles/bin/baseline_xubuntu.sh
-
 # Use an active fork of Compton
 sudo apt install libev-libevent-dev libdbus-1-dev libgl1-mesa-dev \
   libxcb-present-dev libxcb-sync-dev libxcb-damage0-dev \
@@ -49,20 +12,6 @@ cd compton/
 meson --buildtype=release . build
 ninja -C build
 sudo ninja -C build install
-
-cp "${COMMON_HOME}/.config/compton.conf" "${HOME}/.config"
-
-# Set default Rust toolchain
-# shellcheck source=../../bin/baseline_xubuntu.sh
-source "${HOME}"/.cargo/env
-rustup default nightly
-
-# Install Alacritty
-sudo apt install -y libfreetype6-dev
-git clone https://github.com/jwilm/alacritty "${BUILD_DIR}"/alacritty
-cd "${BUILD_DIR}"/alacritty
-cargo build --release
-cp target/release/alacritty "${HOME}"/.local/bin
 
 # i3-gaps
 # Install Deps and Software
@@ -104,19 +53,4 @@ git clone https://github.com/hanschen/ksuperkey.git "${BUILD_DIR}"/ksuperkey
 cd "${BUILD_DIR}"/ksuperkey
 make
 sudo make install
-
-# Copy all configs
-cp -r "${COMMON_HOME}"/.config "${HOME}"/
-cp -r "${MACHINE_HOME}"/.config "${HOME}"/
-
-# Fonts
-  # This download is huge, 1.8G (instead we just get the few fonts we need)
-  #git clone https://github.com/ryanoasis/nerd-fonts ${BUILD_DIR}/nerd-fonts
-  #cd ${BUILD_DIR}/nerd-fonts
-  #./install.sh
-cp -r "${MACHINE_HOME}"/.local/share/fonts "${HOME}"/.local/share
-fc-cache -f -v
-
-# Install Missing ArchLabs Scripts
-cp -r "${COMMON_HOME}"/bin "${HOME}"
 
