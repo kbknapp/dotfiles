4a7ab17624ef0db96df81b3207717f2bd959206f
Kevin K <kbknapp@gmail.com>
2020-04-08 12:39:36 -0400

    fixes for polybar compile and configure

--- bin/common/polybar.sh
+++ bin/common/polybar.sh
@@ -3,10 +3,19 @@
 function f_polybar_main() {
     f_check_prog "git"
     f_check_prog "make"
+
+    f_out "Installing Polybar Build Deps"
+
+    $INSTALL_CMD libcairo2-dev libasound2-dev libcurl4-openssl-dev \
+      libmpdclient-dev libiw-dev libpulse-dev libxcb-composite0-dev \
+      xcb-proto libxcb-icccm4-dev libxcb-ewmh-dev libxcb-util-dev \
+      python3-xcbgen libjsoncpp-dev
+
     f_out "Compiling and installing Polybar"
 
     cd ~/.build/
-    git clone --recurisve https://github.com/jaagr/polybar || true
+    git clone --recursive https://github.com/jaagr/polybar || true
     cd ~/.build/polybar/
     ./build.sh --all-features -g -f
+    cp -r ~/.dotfiles/polybar ~/.config
 }
