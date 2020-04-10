1e19f8b8ddd2ed1ac1401d820bf6c1e10e572890
Kevin K <kbknapp@gmail.com>
2020-04-09 21:59:42 -0400

    fixes pip by not assuming pip3

--- bin/common/rtv.sh
+++ bin/common/rtv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function f_rtv_main() {
-  f_check_prog "pip3"
+  f_check_prog "pip3" false || f_check_prog "pip"
 
   f_out "Installing RTV"
 
-  pip3 install --user --upgrade rtv
+  pip3 install --user --upgrade rtv || pip install --user --upgrade rtv
 }
