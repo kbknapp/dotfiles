4a28dc52dd7f31bba57938b54ce3736692ef5677
Kevin K <kbknapp@gmail.com>
2020-04-09 22:00:31 -0400

    removes duplicate packages

--- bin/fedora/i3.sh
+++ bin/fedora/i3.sh
@@ -6,15 +6,13 @@ function f_i3_pre() {
     sudo dnf install -y libX11-devel libXtst-devel pulseaudio-libs-devel \
         jsoncpp-devel alsa-lib-devel wireless-tools-devel libmpdclient-devel \
         libcurl-devel cairo-devel xcb-proto xcb-util-devel xcb-util-wm-devel \
-        xcb-util-image-devel libxcb-devel xcb-util-keysyms-devel xcb-util-devel \
-        xcb-util-wm-devel xcb-util-xrm-devel yajl-devel libXrandr-devel \
-        startup-notification-devel libev-devel xcb-util-cursor-devel \
+        xcb-util-image-devel libxcb-devel xcb-util-keysyms-devel xcb-util-xrm-devel \
+	yajl-devel startup-notification-devel libev-devel xcb-util-cursor-devel \
         libXinerama-devel libxkbcommon-devel libxkbcommon-x11-devel pcre-devel \
         pango-devel uthash-devel libX11-devel libXcomposite-devel libXfixes-devel \
         libXdamage-devel libXrender-devel libXext-devel libXrandr-devel \
-        libXinerama-devel libconfig-devel pcre-devel libdrm-devel mesa-libGL-devel \
-        dbus-devel asciidoc desktop-file-utils uthash-devel libxdg-basedir-devel \
-        fontconfig-devel freetype-devel meson
+        libconfig-devel libdrm-devel mesa-libGL-devel dbus-devel asciidoc \
+	desktop-file-utils libxdg-basedir-devel fontconfig-devel freetype-devel meson
 
     f_out "Installing i3 Required Programs"
 
--- bin/fedora/misc_small.sh
+++ bin/fedora/misc_small.sh
@@ -3,8 +3,8 @@
 function f_misc_small_main() {
     f_out "Installing various common programs"
 
-    sudo dnf install -y ffmpeg fuse-exfat lm_sensors openssh-askpass p7zip \
-        p7zip-plugins pv python3-devel unar arc-theme youtube-dl meld \
+    sudo dnf install -y ffmpeg fuse-exfat openssh-askpass p7zip \
+        p7zip-plugins python3-devel unar arc-theme youtube-dl meld \
         exfat-utils fuse-sshfs lm_sensors pv util-linux-user wavemon \
         borgbackup iotop nload ncdu xfsprogs
 }
