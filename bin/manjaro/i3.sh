7f3918695b8d580a1f9255b0162d521d40240d8c
Kevin K <kbknapp@gmail.com>
2020-04-10 11:17:42 -0400

    adds options to install picom and i3gaps from AUR

--- bin/manjaro/i3.sh
+++ bin/manjaro/i3.sh
@@ -16,3 +16,31 @@ function f_i3_pre() {
 
     $INSTALL_CMD arandr rofi i3status feh
 }
+
+function f_i3_gaps_aur_pre() {
+    f_out "Installing i3 Required Programs"
+
+    $INSTALL_CMD arandr rofi i3status feh
+}
+
+function f_i3_gaps_aur_main() {
+	f_out "Installing i3 Gaps from AUR"
+
+	if command -v rua 2>/dev/null ;then
+		rua install i3-gaps-next-git
+		return
+	fi
+	if command -v yay 2>/dev/null ;then
+		yay -a -S i3-gaps-next-git
+		return
+	fi
+	if command -v aura 2>/dev/null ;then
+		sudo aura -A i3-gaps-next-git
+	fi
+}
+
+
+function f_i3_post() {
+    f_out "Configuring i3 Gaps"
+
+}
--- bin/manjaro/i3k/picom.sh
+++ bin/manjaro/i3k/picom.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+function f_picom_aur() {
+	f_out "Installing Picom from AUR"
+
+	if command -v rua 2>/dev/null ;then
+		rua install picom-git
+		return
+	fi
+	if command -v yay 2>/dev/null ;then
+		yay -a -S picom-git
+		return
+	fi
+	if command -v aura 2>/dev/null ;then
+		sudo aura -A picom-git
+	fi
+}
+
 function f_picom_post() {
     f_out "Configuring Picom"
     
     cp -r ~/.dotfiles/i3/kde/home/.config/picom/ ~/.config/
 }
+
+function f_picom_aur_post() {
+	f_picome_post
+}
+
