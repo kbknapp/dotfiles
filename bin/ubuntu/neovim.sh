70cfab4a56e63ba9eb609e37ef8456368f263b28
Kevin K <kbknapp@gmail.com>
2020-05-27 12:02:28 -0400

    adds required neovim plugin packages

--- bin/ubuntu/neovim.sh
+++ bin/ubuntu/neovim.sh
@@ -6,7 +6,7 @@ function f_neovim_main() {
     f_out "Installing NeoVIM"
 
     # @TODO download appimage NeoVIM
-    $INSTALL_CMD fzf exuberant-ctags
+    $INSTALL_CMD fzf exuberant-ctags nodejs npm python3-neovim
 
     f_neovim_common
 }
