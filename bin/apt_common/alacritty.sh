1cc820325943f4decf47d38d561e7c1b0a13cba6
Kevin K <kbknapp@gmail.com>
2020-07-16 10:05:19 -0400

    adds missing dep for alacritty on deb distros

--- bin/apt_common/alacritty.sh
+++ bin/apt_common/alacritty.sh
@@ -5,7 +5,8 @@ function f_alacritty_main() {
 
     $INSTALL_CMD fontconfig libfreetype-dev libxcb1-dev \
 	    libxcb-image0-dev libxcb-randr0-dev libxcb-image0-dev \
-	    libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev
+	    libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev \
+      libfontconfig-dev
 
     f_alacritty_common
 }
