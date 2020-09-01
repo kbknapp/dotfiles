b67e9a26bb5f0d4011671be730d4bf0fa79b9062
Kevin K <kbknapp@gmail.com>
2020-08-31 20:53:17 -0400

    fixes copying i3 files when dirs exist"

--- bin/fedora/i3k/i3.sh
+++ bin/fedora/i3k/i3.sh
@@ -5,7 +5,7 @@ function f_i3_gaps_post() {
 
     sudo cp ~/.dotfiles/i3/kde/usr/bin/* /usr/bin/
     sudo cp ~/.dotfiles/i3/kde/etc/* /etc
-    cp -r ~/.dotfiles/i3/common/home/.local/* ~/.local/
+    cp -r ~/.dotfiles/i3/common/home/.local/* ~/.local/ || cp -r ~/.dotfiles/i3/common/home/.local/bin/* ~/.local/bin/ && cp -r ~/.dotfiles/i3/common/home/.local/share/ ~/.local/
     cp -r ~/.dotfiles/i3/common/home/.config/fontconfig/ ~/.config/
     cp -r ~/.dotfiles/i3/kde/home/.config/i3/ ~/.config/
     cp -r ~/.dotfiles/i3/kde/home/.config/rofi ~/.config/
