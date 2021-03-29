6f592e014fbc71b35f881b734d6b791b602a1367
Kevin K <kbknapp@gmail.com>
2021-03-29 17:17:42 -0400

    adds more deadgrep shortcuts

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -69,6 +69,7 @@
 (setq org-directory "~/org")
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 (setq tab-stop-list (number-sequence 4 200 4))
+(setq-default tab-width 4)
 
 (setq shell-file-name "/usr/bin/zsh"
       eshell-aliases-file "~/.doom.d/aliases"
@@ -215,3 +216,160 @@
 
 (add-hook 'before-save-hook (lambda () (when (eq 'rust-mode major-mode)
                                            (lsp-format-buffer))))
+
+
+; ;; Deadgrep
+; ;; https://github.com/Wilfred/deadgrep
+;
+(after! deadgrep
+;  :ensure t
+;  :defer t
+;  :init
+;  (progn
+;    (bind-to-modi-map "r" #'deadgrep))
+;  :config
+;  (progn
+    (setq-default deadgrep--search-type 'regexp) ;Default is 'string
+
+    (defconst modi/deadgrep--rg-extra-args
+      `("--no-ignore-vcs"       ;Ignore files/dirs ONLY from `.ignore'
+        "--follow"              ;Follow symlinks
+        "--hidden"              ;Search hidden files
+        "--ignore-file" ,(expand-file-name ".ignore" (getenv "HOME")))
+      "Extra rg arguments to be added to `deadgrep--format-command' output.")
+
+    ;; Thu Aug 23 15:51:37 EDT 2018 - kmodi
+    ;; Adding extra arguments to the ones
+    ;; already in `deadgrep--format-command' --
+    ;; https://github.com/Wilfred/deadgrep/issues/24
+    (defun modi/deadgrep--format-command-advice (orig-ret-val)
+      "Add arguments from `modi/deadgrep--rg-extra-args' to ORIG-RET-VAL."
+      (let ((new-ret (replace-regexp-in-string
+                      (format "\\`\\(%s \\)\\(.*\\)\\'" (regexp-quote deadgrep-executable))
+                      (concat "\\1"
+                              (mapconcat #'identity modi/deadgrep--rg-extra-args " ")
+                              " \\2")
+                      orig-ret-val)))
+        (message "[Deadgrep] %s" new-ret)
+        new-ret))
+    (advice-add 'deadgrep--format-command :filter-return #'modi/deadgrep--format-command-advice)
+
+    (defun modi/deadgrep--jump-to-and-execute (re)
+      "Execute the button that matches RE and push it."
+      (goto-char (point-min))
+      (re-search-forward re)
+      (backward-char 3)
+      (push-button))
+
+    (defun modi/deadgrep-change-search-term ()
+      "Change the search term."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Search term: .*change$"))
+
+    (defun modi/deadgrep-change-search-type-to-string ()
+      "Change the search type to 'string'."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Search type: .*string"))
+
+    (defun modi/deadgrep-change-search-type-to-words ()
+      "Change the search type to 'words'."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Search type: .*words"))
+
+    (defun modi/deadgrep-change-search-type-to-regexp ()
+      "Change the search type to 'regexp'."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Search type: .*regexp"))
+
+    (defun modi/deadgrep-change-case-to-smart ()
+      "Change the case sensitivity to 'smart'."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Case: .*smart"))
+
+    (defun modi/deadgrep-change-case-to-sensitive ()
+      "Change the case sensitivity to 'sensitive'."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Case: .*sensitive"))
+
+    (defun modi/deadgrep-change-case-to-ignore ()
+      "Change the case sensitivity to 'ignore'."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Case: .*ignore"))
+
+    (defun modi/deadgrep-change-context-to-none ()
+      "Don't show ny context around the search results."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Context: .*none"))
+
+    (defun modi/deadgrep-change-context-to-before ()
+      "Set 'before' context for the search results."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Context: .*before"))
+
+    (defun modi/deadgrep-change-context-to-after ()
+      "Set 'after' context for the search results."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Context: .*after"))
+
+    (defun modi/deadgrep-change-directory ()
+      "Change the root directory for searches."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Directory: .*$"))
+
+    (defun modi/deadgrep-search-all-files ()
+      "Change file search scope to 'all'."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Files: .*all"))
+
+    (defun modi/deadgrep-search-files-by-type ()
+      "Search only in the specified file types."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Files: .*type"))
+
+    (defun modi/deadgrep-search-files-by-glob ()
+      "Search in files names that match the specified glob."
+      (interactive)
+      (modi/deadgrep--jump-to-and-execute "^Files: .*glob")))
+
+(map! :after deadgrep
+      :map deadgrep-mode
+      :leader
+      (:prefix-map ("s g" . "ripgrep")
+      :leader
+      :desc "Restart" "r" #'deadgrep-restart
+      :leader
+      :desc "Change Term" "s" #'modi/deadgrep-change-search-term
+      :leader
+      :desc "Change Directory" "d" #'modi/deadgrep-change-directory
+      :leader
+      (:prefix-map ("c" . "Case")
+        :leader
+        :desc "Ignore" "i" #'modi/deadgrep-change-case-to-ignore
+        :leader
+        :desc "Smart" "s" #'modi/deadgrep-change-case-to-smart
+        :leader
+        :desc "Smart" "S" #'modi/deadgrep-change-case-to-sensitive)
+      :leader
+      (:prefix-map ("t" . "Type")
+        :leader
+        :desc "String" "s" #'modi/deadgrep-change-search-type-to-string
+        :leader
+        :desc "Words" "w" #'modi/deadgrep-change-search-type-to-words
+        :leader
+        :desc "Regex" "r" #'modi/deadgrep-change-search-type-to-regexp)
+      :leader
+      (:prefix-map ("f" . "Files")
+        :leader
+        :desc "All" "a" #'modi/deadgrep-search-all-files
+        :leader
+        :desc "Type" "t" #'modi/deadgrep-search-files-by-type
+        :leader
+        :desc "Glob" "g" #'modi/deadgrep-search-files-by-glob)
+      :leader
+      (:prefix-map ("C" . "Context")
+        :leader
+        :desc "Before" "b" #'modi/deadgrep-change-context-to-before
+        :leader
+        :desc "After" "a" #'modi/deadgrep-change-context-to-after
+        :leader
+        :desc "None" "n" #'modi/deadgrep-change-context-to-none)))
