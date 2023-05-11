3ca12aa3dfb9d0ea8b4cf907617e38e8a418d0ab
Kevin K <kbknapp@gmail.com>
2023-05-11 13:12:29 -0400

    add target dir to telescope ignore

--- nvim/init.lua
+++ nvim/init.lua
@@ -342,7 +342,7 @@ require('telescope').setup{
       },
     },
     file_sorter =  require'telescope.sorters'.get_fuzzy_file,
-    file_ignore_patterns = {},
+    file_ignore_patterns = { "target" },
     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
     winblend = 0,
     border = {},
