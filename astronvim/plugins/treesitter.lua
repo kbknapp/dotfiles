79deffecd690993986ba38403b0a43178fe6b0fc
Kevin K <kbknapp@gmail.com>
2023-06-06 16:06:26 -0400

    astronvim

--- astronvim/plugins/treesitter.lua
+++ astronvim/plugins/treesitter.lua
@@ -3,7 +3,17 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "bash",
+      "dockerfile",
+      "hcl",
+      "http",
+      "json",
+      "markdown",
+      "python",
+      "rust",
+      "sql",
+      "toml",
+      "yaml"
     })
   end,
 }
--- astronvim/plugins/user.lua
+++ astronvim/plugins/user.lua
@@ -1,4 +1,8 @@
 return {
+  {"NoahTheDuke/vim-just", event = "BufEnter *.just,justfile"},
+  "bitc/vim-bad-whitespace",
+  "kbknapp/badWords",
+  {'NTBBloodbath/rest.nvim', event = "BufEnter *.http"},
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
