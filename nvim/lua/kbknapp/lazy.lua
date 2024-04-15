ccfbf24ce19b303a0e3e9c2a71184b6f1624490f
Kevin K. <kevin@24dev.io>
2024-04-15 14:25:53 -0400

    remove rest.nvim for now

--- nvim/lua/kbknapp/lazy.lua
+++ nvim/lua/kbknapp/lazy.lua
@@ -62,8 +62,17 @@ require("lazy").setup({
   },
 
   {
-    "rest-nvim/rest.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "mfussenegger/nvim-dap",
+      {
+        "lvimuser/lsp-inlayhints.nvim",
+        opts = {}
+      },
+    },
+    ft = { "rust" },
   },
 
   {
