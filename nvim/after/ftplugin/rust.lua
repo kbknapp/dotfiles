3f9854480f76c0762bcd18a21ee36413d15f6a93
Kevin K <kevin@24dev.io>
2024-04-18 14:56:06 -0400

    add more rust keybindings

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -1,12 +1,13 @@
 local bufnr = vim.api.nvim_get_current_buf()
+local opts = { silent = true, buffer = bufnr }
 vim.keymap.set(
-  "n",
+{"n", "v"},
   "<leader>a",
   function()
     vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
     -- or vim.lsp.buf.codeAction() if you don't want grouping.
   end,
-  { silent = true, buffer = bufnr }
+  opts
 )
 
 vim.keymap.set(
@@ -15,14 +16,28 @@ vim.keymap.set(
   function()
     vim.cmd.RustLsp('expandMacro')
   end,
-  { silent = true, buffer = bufnr }
+  opts
 )
 
 vim.keymap.set(
-  "n",
+{"n", "v"},
   "<leader>aa",
   function()
     vim.cmd.RustLsp{ 'hover', 'actions' }
   end,
-  { silent = true, buffer = bufnr }
+  opts
 )
+
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+--vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+--vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+--vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+--vim.keymap.set('n', '<leader>wl', function()
+--  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+--end, opts)
+vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+--vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
